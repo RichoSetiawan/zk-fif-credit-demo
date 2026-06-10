f814ce43a463baac3ebeae38ec5cf5b194ca2c62
anovandri <aditya.novandri@tech.jago.com>
2026-06-10 11:38:51 +0700

    initial commit

---
+++ src/main/java/com/fif/services/impl/DatabaseLogRepository.java
@@ -0,0 +1,35 @@
+package com.fif.services.impl;
+
+import com.fif.entity.Log;
+import com.fif.services.LogRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class DatabaseLogRepository implements LogRepository {
+
+    @Autowired
+    private DatabaseLogJpaRepository jpaRepository;
+
+    @Override
+    public List<Log> queryAll() {
+        return jpaRepository.findAll();
+    }
+
+    @Override
+    public Log get(String id) {
+        return jpaRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Log save(Log log) {
+        return jpaRepository.save(log);
+    }
+
+    @Override
+    public void delete(Log log) {
+        jpaRepository.delete(log);
+    }
+}
